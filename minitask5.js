4ecfb8ade2f32bb45a22c4906c8d68e6713744ea
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-17 14:59:04 +0700

    Add files via upload

---
+++ minitask5.js
@@ -0,0 +1,22 @@
+const konversi = (c, type, callback) => {
+  if (typeof c !== "number") {
+    console.log("harap masukkan angka");
+    return;
+  }
+  if (type === "f") {
+    return callback(c);
+  } else if (type === "r") {
+    return callback(c);
+  } else if (type === "k") {
+    return callback(c);
+  } else {
+    console.log("harap masukkan F,R atau K");
+  }
+};
+let result = null;
+const farenheit = (c) => (result = c * 1.8 + 32);
+const reamur = (c) => (result = (c * 4) / 5);
+const kelvin = (c) => (result = c + 273);
+console.log(konversi(10, "f", farenheit));
+console.log(konversi(10, "r", reamur));
+console.log(konversi(20, "k", kelvin));
