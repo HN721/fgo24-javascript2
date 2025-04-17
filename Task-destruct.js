3858379026699ad14965bc66a9c24c93e9db0554
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-17 11:06:27 +0700

    Update Task-destruct.js

--- Task-destruct.js
+++ Task-destruct.js
@@ -19,7 +19,21 @@ const data = [
 ];
 // console.log(data[0][1][2][0].string[1].value);
 
-const {
-  string: [, { value }],
-} = data[0][1][2][0];
+const [
+  [
+    _x,
+    [
+      _i,
+      _j,
+      [
+        {
+          string: [test, { value }],
+        },
+      ],
+    ],
+  ],
+] = data;
+
+console.log(value); // "hallo"
+
 console.log(value);
