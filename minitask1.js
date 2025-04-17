5270b3530c806d526c908143ffe9cb94a8fa2a92
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-17 11:33:40 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,10 @@
+function checkNumber(number) {
+  if (number % 2 === 0) {
+    console.log(`$Bilangan Genap`);
+  } else {
+    console.log(`Bilangan Ganjil`);
+  }
+}
+checkNumber(10);
+checkNumber(7);
+checkNumber(8);
