1a40602b206ae0ddeab3a62d12436c5be0990f4c
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-17 13:44:30 +0700

    Update minitask3.js

--- minitask3.js
+++ minitask3.js
@@ -3,20 +3,27 @@ const temperature = {
     return c * 1.8 + 32;
   },
   reamur: function (c) {
-    return c * 4 / 5;
+    return (c * 4) / 5;
   },
   kelvin: function (c) {
     return c + 273;
   },
-  total: function (c) {
-    return {
-      farenheit: this.farenheit(c),
-      reamur: this.reamur(c),
-      kelvin: this.kelvin(c),
-    };
+  total: function (c, selector) {
+    if (typeof c !== "number") {
+      console.log("harap masukkan angka");
+    }
+    if (selector === "F") {
+      return this.farenheit(c);
+    } else if (selector === "R") {
+      return this.reamur(c);
+    } else if (selector === "K") {
+      return this.kelvin(c);
+    } else {
+      console.log("harap masukkan F,R atau K");
+    }
   },
 };
-console.log(temperature.total(2));
-console.log(temperature.total(10));
-console.log(temperature.total(20));
-console.log(temperature.total(30));
+console.log(temperature.total(2, "F"));
+console.log(temperature.total(10, "R"));
+console.log(temperature.total(20, "K"));
+console.log(temperature.total(30, "K"));
