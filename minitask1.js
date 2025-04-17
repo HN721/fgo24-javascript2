67f2d08b46ea050843c8d938f8064789ea60431b
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-17 11:35:32 +0700

    Update minitask1.js

--- minitask1.js
+++ minitask1.js
@@ -1,6 +1,6 @@
 function checkNumber(number) {
   if (number % 2 === 0) {
-    console.log(`$Bilangan Genap`);
+    console.log(`Bilangan Genap`);
   } else {
     console.log(`Bilangan Ganjil`);
   }
